61a0e7fb437cfb35969518e4f0e00d408a9af096
Mili Antoni <miliantoni12@gmail.com>
2024-11-03 19:28:46 -0300

    .

--- routes/roomsRoute.js
+++ routes/roomsRoute.js
@@ -2,17 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Room = require('../models/room');
 
-
-router.get("/getallrooms", async (req, res) => {
-   try {
-       const rooms = await Room.find({});
-       return res.json({ rooms });
-   } catch (error) {
-       return res.status(400).json({ message: error.message });
-   }
+// Endpoint para obtener todas las habitaciones
+router.get('/getallrooms', async (req, res) => {
+    try {
+        const rooms = await Room.find(); // Busca todas las habitaciones
+        res.json(rooms); // Devuelve la lista de habitaciones como JSON
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching rooms" });
+    }
 });
 
-
 module.exports = router;
+
 
 
